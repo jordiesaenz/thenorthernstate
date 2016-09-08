17458f332b5f12cc9fcc4eb1244109577801227a
Jordie Saenz <jordie.saenz@gmail.com>
2016-09-08 01:21:20 -0700

    first blog post

--- _site/scripts/header.js
+++ _site/scripts/header.js
@@ -1,6 +1,5 @@
 $(function() {
     $("#menuDropdown").click( function() {
-      console.log("CLICK");
       $(".menu").toggleClass("open");
       $("#dropdownIcon").toggleClass("fa-caret-down");
       $("#dropdownIcon").toggleClass("fa-close");
