c203b082e9591a4a4c9f692ee6da2a0b38af2351
Jordie Saenz <jordie.saenz@gmail.com>
2016-09-23 23:49:32 -0700

    adds new optin popup

---
+++ scripts/optin.js
@@ -0,0 +1,8 @@
+$( document ).ready(function() {
+    $("#optin-close").click(function() {
+      $(".optin-overlay").slideToggle("fast");
+    });
+    $("#optin-open").click(function() {
+      $(".optin-overlay").slideToggle("fast");
+    });
+});
