d0041cac1ab868a2bbb211f2341e1649b1471fb9
Jordie Saenz <jordie.saenz@gmail.com>
2016-09-24 13:02:56 -0700

    adds shake effect to optin button

--- scripts/optin.js
+++ scripts/optin.js
@@ -5,4 +5,16 @@ $( document ).ready(function() {
     $("#optin-open").click(function() {
       $(".optin-overlay").slideToggle("fast");
     });
+    jQuery.fn.shake = function() {
+      this.each(function(i) {
+        $(this).css({ "position" : "relative" });
+        for (var x = 1; x <= 3; x++) {
+          $(this).animate({ left: -50 }, 50).animate({ left: 0 }, 50).animate({ left: 50 }, 50).animate({ left: 0 }, 50);
+        }
+      });
+      return this;
+    }
+    setInterval(function(){
+      $('#optin-open').shake();
+    },5000);
 });
